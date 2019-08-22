cc21a4ea47f813d5604b4a8aa06c453453a7abab
gokulkp <gokul.pillai98@gmail.com>
2019-08-22 12:10:33 +0530

    allsparks

--- public/scripts/motiondetection.js
+++ public/scripts/motiondetection.js
@@ -1,31 +1,32 @@
-var deviceorientation = window.addEventListener(
-  "deviceorientation",
-  handleOrientation,
-  false
-);
-function handleOrientation(event) {
-  // var absolute = event.absolute;
-  // var alpha = event.alpha;
-  // var beta = event.beta;
-  // var gamma = event.gamma;
-  // console.log("Device Orientation ", absolute, alpha, beta, gamma);
-}
-
-if (window.DeviceMotionEvent) {
-  console.log("device motion");
-  window.addEventListener("devicemotion", motion, false);
+var flag = 1;
+if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", handleOrientation, false);
 } else {
-  console.log("DeviceMotionEvent is not supported");
+  $("body").append("<p>DeviceOrientation is not supported</p>");
 }
 
-function motion(event) {
-console.log(event)
-  // console.log(
-  //   "Accelerometer: " +
-  //     event.accelerationIncludingGravity.x +
-  //     ", " +
-  //     event.accelerationIncludingGravity.y +
-  //     ", " +
-  //     event.accelerationIncludingGravity.z
-  // );
+function handleOrientation(event) {
+  if (Math.abs(event.gamma) > 75 && Math.abs(event.gamma) < 90 && flag) {
+    if (window.DeviceMotionEvent) {
+      $("body").append("<p>Added divce motion listener</p>");
+      window.addEventListener("devicemotion", handleMotion, false);
+      flag = 0;
+    } else {
+      $("body").append("<p>DeviceMotion is not supported</p>");
+    }
+  }
+}
+let moveCounter = 0;
+function handleMotion(e) {
+  let acc = e.acceleration;
+  if (!acc.x) return;
+  if (Math.abs(acc.x) >= 5 && Math.abs(acc.y) >= 5) {
+    moveCounter++;
+    $("body").append("<p>" + moveCounter + "</p>");
+    if (moveCounter > 4) {
+      $("body").append("<p>brochure is ready</p>");
+      window.removeEventListener("devicemotion", handleMotion, false);
+      moveCounter = 0;
+    }
+  }
 }
